4367b7bdc975f663b47edbaba55ab920b4b991c8
BOSS-Danuphan <bossmeej@gmail.com>
2019-01-19 18:02:04 +0000

    Add log to scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -5,8 +5,10 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def minutes_job():
+    print('This job is run every minutes.', datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     with open('scheduler-log.txt', 'w+') as f:
+        print('Writing log: start', datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
         f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    print('This job is run every minutes.')
+        print('Writing log: done', datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 sched.start()
